81dba7247b73ac10342db59c3659e99280bce17f
Simon Ding <me@simonding.de>
2017-06-27 17:15:11 +0200

    Do not try to transform imports of files

---
+++ test/fixtures/fileWildcard/expected.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+var _myModule = require('./my-module.js');
+
+var all = _interopRequireWildcard(_myModule);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+console.log(all.hello);
---
+++ test/fixtures/fileWildcard/my-module.js
@@ -0,0 +1,1 @@
+export let hello = 'Hello World!';
