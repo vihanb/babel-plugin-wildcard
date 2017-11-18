16adab51960002863fd7e01f93b4050b0251179c
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-11-17 17:30:28 -0800

    Added an additional test to double check recursive multiple importing.

---
+++ test/fixtures/recursive-mixed-multiple/actual.js
@@ -0,0 +1,7 @@
+import * as files from './data/';
+console.log(files.Test());
+console.log(files.Another.Deep.Test());
+console.log(files.Another.Second());
+console.log(files.Another.Test());
+console.log(files.More.Deep.Test());
+console.log(files.More.Deep.Second());
